d0e9ff2f7220b287f08913ac7874338f511b30fc
Andrés <Andrs@192.168.1.109>
2015-11-22 22:33:53 -0500

    Protocolo BDD version 3.3

--- mavenproject1/ProtocoloBDD/src/main/java/com/espe/distribuidas/pmaldito/protocolobdd/test/Test.java
+++ mavenproject1/ProtocoloBDD/src/main/java/com/espe/distribuidas/pmaldito/protocolobdd/test/Test.java
@@ -48,14 +48,14 @@ public class Test {
         lista.add("correo");
         ArrayList listaValor = new ArrayList();
         listaValor.add("CEDUL");
-        listaValor.add("1726247958");
-        listaValor.add("juan");
+        listaValor.add("1726247945");
+        listaValor.add("david");
         listaValor.add("perez");
-        listaValor.add("QuitoSur");
-        listaValor.add("0984383260");
-        listaValor.add("022629564");
-        listaValor.add("12345@alguien.com");
-        listaValor.add("19920701");
+        listaValor.add("conocoto");
+        listaValor.add("0984347860");
+        listaValor.add("022621234");
+        listaValor.add("123@alguien.com");
+        listaValor.add("19620701");
         ins.setValorCamposTabla(MensajeBDD.unirCamposTabla(listaValor));
         MensajeRQ ms = new MensajeRQ(Originador.getOriginador(Originador.SRV_APLICACION), MensajeBDD.idMensajeInsertar);
         ms.setCuerpo(ins);
@@ -77,22 +77,32 @@ public class Test {
         cuerpoFact.add("pollo");
         cuerpoFact.add("5");
         cuerpoFact.add("15");
-        InsertarRQ rq=new InsertarRQ();
+        InsertarRQ rq = new InsertarRQ();
         rq.setNombreTabla("FACTURA");
         rq.setValorCamposTabla(MensajeBDD.unirCamposTabla(cabeceraFact));
         rq.setValorcuerpo(MensajeBDD.unirCamposTabla(cuerpoFact));
-        MensajeRQ rs=new MensajeRQ(Originador.getOriginador(Originador.SRV_APLICACION), MensajeBDD.idMensajeInsertar);
+        MensajeRQ rs = new MensajeRQ(Originador.getOriginador(Originador.SRV_APLICACION), MensajeBDD.idMensajeInsertar);
         rs.setCuerpo(rq);
         System.out.println(rs.asTexto());
         ///rq info cliente
-        ConsultarRQ crq=new ConsultarRQ();
+        ConsultarRQ crq = new ConsultarRQ();
         crq.setNombreTabla("CLIENTE");
         crq.setCamposTabla("/");
         crq.setCodigoIdentificadorColumna("1");
-        crq.setValorCodigoidentificadorColumna("1726247958");
-        MensajeRQ rqd=new MensajeRQ(Originador.getOriginador(Originador.SRV_APLICACION), MensajeBDD.idMensajeConsultar);
+        crq.setValorCodigoidentificadorColumna("1707412548");
+        MensajeRQ rqd = new MensajeRQ(Originador.getOriginador(Originador.SRV_APLICACION), MensajeBDD.idMensajeConsultar);
         rqd.setCuerpo(crq);
         System.out.println(rqd.asTexto());
+        //rq producto
+        ConsultarRQ crq1 = new ConsultarRQ();
+        crq.setNombreTabla("PRODUCTO");
+        crq.setCamposTabla("/");
+        crq.setCodigoIdentificadorColumna("0");
+        crq.setValorCodigoidentificadorColumna("1235");
+        MensajeRQ rqd1 = new MensajeRQ(Originador.getOriginador(Originador.SRV_APLICACION), MensajeBDD.idMensajeConsultar);
+        rqd.setCuerpo(crq);
+        System.out.println(rqd.asTexto());
+
     }
 
 }
